d1393a2798c4c46deee2f2960bb0b26942e6625a
Gio Palacino <gio@palacino.net>
2022-02-07 08:42:20 -0600

    enhance(iam): outputs lambda exec role arn and id

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,12 @@
 # Available Output Values
 #################################################
 
+output "lambda_exec_role_arn" {
+  value = aws_iam_role.lambda_exec_role.arn
+  description = "The Lambda Execution Role ARN. Useful for granting additional permission like data access."
+}
+
+output "lambda_exec_role_id" {
+  value = aws_iam_role.lambda_exec_role.id
+  description = "The Lambda Execution Role ID (role name). Useful for granting additional permission like data access."
+}
