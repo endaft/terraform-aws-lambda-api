e24ac0ecfef5b9f74e38bc5c3046ff5101fc8b3b
Gio Palacino <gio@palacino.net>
2022-04-22 08:06:24 -0500

    fix(logging): sets explicit log permissions

--- iam.tf
+++ iam.tf
@@ -31,3 +31,28 @@ data "aws_iam_policy_document" "lambda_arp_doc" {
     }
   }
 }
+
+resource "aws_iam_policy" "lambda_logging" {
+  name_prefix = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+data "aws_iam_policy_document" "lambda_logging" {
+  version = "2012-10-17"
+  statement {
+    effect    = "Allow"
+    resources = ["arn:aws:logs:*:*:*"]
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
