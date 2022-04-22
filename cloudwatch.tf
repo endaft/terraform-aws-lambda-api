5f445e8dc94d05a8f1047cc293eac5a4802a8627
Gio Palacino <gio@palacino.net>
2022-04-22 07:59:26 -0500

    fix(gateway): adds gateway function log group

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -13,3 +13,10 @@ resource "aws_cloudwatch_log_group" "api_gateway" {
   name              = "/aws/api_gw/${aws_apigatewayv2_api.app.name}"
   retention_in_days = var.log_retention_days
 }
+
+resource "aws_cloudwatch_log_group" "cf_lambda_logs" {
+  count = local.web_apps_count > 1 ? 1 : 0
+
+  name              = "/aws/lambda/${aws_lambda_function.cloudfront[0].function_name}"
+  retention_in_days = var.log_retention_days
+}
