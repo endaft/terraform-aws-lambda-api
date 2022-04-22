464d823f24ae4bbc2f94f5fb771029ee2537c984
Gio Palacino <gio@palacino.net>
2022-04-22 12:08:03 -0500

    feat(cloudfront): improves bucket security

--- s3.tf
+++ s3.tf
@@ -11,6 +11,15 @@ resource "aws_s3_bucket_acl" "app" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "app" {
+  bucket = aws_s3_bucket.app.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "app" {
   bucket = aws_s3_bucket.app.bucket
 
