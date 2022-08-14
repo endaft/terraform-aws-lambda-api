500305d70bb8769166903dc7b0bea5660c3c4c13
Gio Palacino <gio@palacino.net>
2022-08-14 10:10:13 -0700

    feat: adds CloudFront outputs

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,13 @@ output "lambda_exec_role_id" {
   value = aws_iam_role.lambda_exec_role.id
   description = "The Lambda Execution Role ID (role name). Useful for granting additional permission like data access."
 }
+
+output "cloudfront_dist_id" {
+  value = aws_cloudfront_distribution.app.id
+  description = "The CloudFront Distribution ID. Useful for automated invalidations."
+}
+
+output "cloudfront_dist_arn" {
+  value = aws_cloudfront_distribution.app.arn
+  description = "The CloudFront Distribution ARN. Useful for a variety of operations."
+}
