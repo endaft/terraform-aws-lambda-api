ea4108afc90c072d31b0f533ec08ae5fd44dbe49
Gio Palacino <gio@palacino.net>
2022-08-24 10:08:39 -0700

    fix(api-gateway): adds error context

--- api_gateway.tf
+++ api_gateway.tf
@@ -37,16 +37,19 @@ resource "aws_apigatewayv2_stage" "app" {
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway.arn
     format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
+      errorMessage            = "$context.error.message"
       httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
       integrationErrorMessage = "$context.integrationErrorMessage"
+      integrationStatus       = "$context.integration.status"
+      protocol                = "$context.protocol"
+      requestId               = "$context.requestId"
+      requestTime             = "$context.requestTime"
+      resourcePath            = "$context.resourcePath"
+      responseLength          = "$context.responseLength"
+      routeKey                = "$context.routeKey"
+      sourceIp                = "$context.identity.sourceIp"
+      stage                   = "$context.stage"
+      status                  = "$context.status"
       }
     )
   }
